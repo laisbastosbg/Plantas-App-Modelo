cb360766b9db9d6609bc606f8f75df0b2f4449ab
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2025-01-22 14:21:50 -0300

    ✨ feat: Adiciona tela inicial com carrossel para escolha de tutorial de plantas
    - Implementa `ContentView` com carrossel horizontal utilizando `ScrollView`.
    - Adiciona componente `PlantOption` para exibir as opções de plantas (manjericão, coentro e cebolinha).
    - Configura layout responsivo com animações de transição para cada item no carrossel.
    - Botão "Aprender" adicionado para futuras ações de navegação.

--- Plantas-App-Modelo/ContentView.swift
+++ Plantas-App-Modelo/ContentView.swift
@@ -9,16 +9,61 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
+//        NavigationStack {
+            ScrollView(.horizontal) {
+                HStack(spacing: 25) {
+                    PlantOption(index: "I", title: "Manjericão", image: "Basil-PLACEHOLDER", buttonText: "Aprender")
+                    
+                    PlantOption(index: "II", title: "Coentro", image: "Coriander-PLACEHOLDER", buttonText: "Aprender")
+                    
+                    PlantOption(index: "III", title: "Cebolinha", image: "Scallions-PLACEHOLDER", buttonText: "Aprender")
+                }
+                .scrollTargetLayout()
+            }
+//            .contentMargins(50, for: .scrollContent)
+            .scrollTargetBehavior(.viewAligned)
+//        }
+    }
+}
+
+struct PlantOption: View {
+    
+    var index: String
+    
+    var title: String
+    
+    var image: String
+    
+    var buttonText: String
+    
+    var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(index)
+                .font(.largeTitle)
+                .bold()
+            Text(title)
+                .font(.title)
+            Image(image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: UIScreen.main.bounds.width - 50)
+            Button(action: {}, label: {
+                Text(buttonText)
+            })
+            .padding()
+            .border(.accent)
         }
         .padding()
+        .scrollTransition { content, phase in
+            content
+                .opacity(phase.isIdentity ? 1 : 0.5) // Apply opacity animation
+                .scaleEffect(y: phase.isIdentity ? 1 : 0.7) // Apply scale animation
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+
+
